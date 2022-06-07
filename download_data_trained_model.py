af0aebef8e7dae181bc99071975569d61914aa49
YJonmo <y.jonmo@gmail.com>
2022-06-08 01:01:01 +1000

    Update download_data_trained_model.py

--- download_data_trained_model.py
+++ download_data_trained_model.py
@@ -8,11 +8,11 @@ Vars = globalVars.parse()
 
 
 if not os.path.exists(Vars.data_path):
-    os.mkdir(Vars.data_CTtraining)
+    os.mkdir(Vars.data_path)
 
-gdown.download('https://drive.google.com/uc?export=download&id=1DXBQfRB2aczpSX_GUyRr4TxE7bflpc8g', os.path.join(globalVars.data_path, 'resnet50_4_2_40.pt'))
-gdown.download('https://drive.google.com/uc?export=download&id=1VTUXanL66UL_YNN9twaWX1a0nspEhQb2', os.path.join(globalVars.data_path, 'training_data.pkl'))
-gdown.download('https://drive.google.com/uc?export=download&id=18oK4RWmfACHVo6PtbZshYcBxiIiDC65L', os.path.join(globalVars.data_path, 'testing_data.pkl'))
+gdown.download('https://drive.google.com/uc?export=download&id=18oK4RWmfACHVo6PtbZshYcBxiIiDC65L', os.path.join(Vars.save_path, 'resnet50_4_2_40.pt'))
+gdown.download('https://drive.google.com/uc?export=download&id=1VTUXanL66UL_YNN9twaWX1a0nspEhQb2', os.path.join(Vars.data_path, 'testing_data.pkl'))
+gdown.download('https://drive.google.com/uc?export=download&id=1DXBQfRB2aczpSX_GUyRr4TxE7bflpc8g', os.path.join(Vars.data_path, 'training_data.pkl'))
 
 #ROOT = os.getcwd()
 
