04b86ccc495187705be67a0036b9a363707216a3
YJonmo <y.jonmo@gmail.com>
2022-06-08 01:02:15 +1000

    Update download_data_trained_model.py

--- download_data_trained_model.py
+++ download_data_trained_model.py
@@ -9,7 +9,11 @@ Vars = globalVars.parse()
 
 if not os.path.exists(Vars.data_path):
     os.mkdir(Vars.data_path)
+    
+if not os.path.exists(Vars.save_path):
+    os.mkdir(Vars.save_path)
 
+    
 gdown.download('https://drive.google.com/uc?export=download&id=18oK4RWmfACHVo6PtbZshYcBxiIiDC65L', os.path.join(Vars.save_path, 'resnet50_4_2_40.pt'))
 gdown.download('https://drive.google.com/uc?export=download&id=1VTUXanL66UL_YNN9twaWX1a0nspEhQb2', os.path.join(Vars.data_path, 'testing_data.pkl'))
 gdown.download('https://drive.google.com/uc?export=download&id=1DXBQfRB2aczpSX_GUyRr4TxE7bflpc8g', os.path.join(Vars.data_path, 'training_data.pkl'))
