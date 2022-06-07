cf5acd6e0f8a408dc6f6d3360a95918f92e62726
Yaqub Jonmohamadi <yaqub.jonmohamadi@mport.com.au>
2022-06-08 00:50:45 +1000

    g

--- download_data_trained_model.py
+++ download_data_trained_model.py
@@ -4,10 +4,11 @@ from pathlib import Path
 from utils.ClassifierOptions import Options
 
 globalVars = Options()
+Vars = globalVars.parse()
 
 
-if not os.path.exists(globalVars.data_CTtraining):
-    os.mkdir(globalVars.data_CTtraining)
+if not os.path.exists(Vars.data_path):
+    os.mkdir(Vars.data_CTtraining)
 
 gdown.download('https://drive.google.com/uc?export=download&id=1DXBQfRB2aczpSX_GUyRr4TxE7bflpc8g', os.path.join(globalVars.data_path, 'resnet50_4_2_40.pt'))
 gdown.download('https://drive.google.com/uc?export=download&id=1VTUXanL66UL_YNN9twaWX1a0nspEhQb2', os.path.join(globalVars.data_path, 'training_data.pkl'))
